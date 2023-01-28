1ffbac172014cfa9149c55bbb294ae7a7b290d80
Ghala Al-melfi <122043754+GhalaMoh4@users.noreply.github.com>
2023-01-28 18:10:50 +0300

    Add files via upload

---
+++ Assets/Blade.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour {
+
+	public GameObject bladeTrailPrefab;
+	public float minCuttingVelocity = .001f;
+
+	bool isCutting = false;
+
+	Vector2 previousPosition;
+	
+	GameObject currentBladeatrail;
+
+	Rigidbody2D rb;
+	Camera cam;
+	CircleCollider2D circleCollider;
+
+	void Start ()
+	{
+		cam = Camera.main;
+		rb = GetComponent<Rigidbody2D>();
+		circleCollider = GetComponent<CircleCollider2D>();
+	}
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			StartCutting();
+		} else if (Input.GetMouseButtonUp(0))
+		{
+			StopCutting();
+		}
+		if (isCutting)
+		{
+			UpdateCut();
+		}
+	}
+	void UpdateCut ()
+	{
+		Vector2 newPosition =  cam.ScreenToWorldPoint(Input.mousePosition);
+		rb.position = newPosition;
+
+		float velocity = (newPosition - previousPosition).magnitude / Time.deltaTime;
+		if(velocity > minCuttingVelocity)
+
+		{
+			circleCollider.enabled = true;
+		} else
+		{
+			circleCollider.enabled = false;
+		}
+
+		previousPosition = newPosition;
+	}
+	void StartCutting()
+	{
+		isCutting = true;
+		currentBladeatrail = Instantiate(bladeTrailPrefab, transform);
+		previousPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+		circleCollider.enabled = false;
+
+	}
+	void StopCutting()
+	{
+		isCutting = false;
+		currentBladeatrail.transform.SetParent(null);
+		Destroy(currentBladeatrail, 2f);
+		circleCollider.enabled = false;
+	}
+}
